2206aa856dc45dee5330ece2182389a5a3b4eafd
Navil <Navil@DESKTOP-6CJ4T69>
2025-06-15 18:18:51 +0100

    java programs

--- C2Tcjavapgrms/src/day1/Addition_java.java
+++ C2Tcjavapgrms/src/day1/Addition_java.java
@@ -1,21 +1,8 @@
-package day1;
-
 public class Addition_java {
     public static void main(String[] args) {
-       
-        if (args.length < 2) {
-            System.out.println("Error: Please provide two numbers as arguments.");
-            return;
-        }
-
-        try {
-            int a = Integer.parseInt(args[0]); 
-            int b = Integer.parseInt(args[1]);
-            int c = a + b; 
-
-            System.out.println("Addition of a=" + a + " and b=" + b + " is " + c);
-        } catch (NumberFormatException e) {
-            System.out.println("Error: Please enter valid integer numbers.");
-        }
+        int a = Integer.parseInt(args[0]); 
+        int b = Integer.parseInt(args[1]);
+        int c = a + b;
+        System.out.println("Addition of a=" + a + " and b=" + b + " is " + c);
     }
 }
---
+++ C2Tcjavapgrms/src/day2/IfDemojava.java
@@ -0,0 +1,17 @@
+package day2;
+
+public class IfDemojava {
+
+	public static void main(String[] args) {
+		int i=5;
+			if(i%2!=0) {
+				System.out.println("the give number is odd");
+			}else {
+				System.out.println("the given number is even");
+			}
+		
+		
+
+	}
+
+}
